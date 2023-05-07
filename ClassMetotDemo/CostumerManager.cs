40e31348fb9e36bd135222586578422490758716
tugba <tugba@TUBA>
2023-05-07 14:31:07 +0300

    Add project files.

---
+++ ClassMetotDemo/CostumerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ClassMetotDemo
+{
+    class CostumerManager
+    {
+        public void Add(Costumer costumer)
+        {
+            Console.WriteLine(costumer.Id + costumer.Name + " Eklendi.");
+        }
+        public void List(Costumer costumer)
+        {
+            Console.WriteLine(costumer.Id + costumer.Name + " Listelendi." );
+        }
+        public void Delete(Costumer costumer)
+        {
+            Console.WriteLine(costumer.Id + " Kayıt silindi");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ClassMetotDemo
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Costumer costumer1 = new Costumer
+            {
+                Id = 1,
+                IdentificationNumber = 1111111111,
+                Name = "Tuba",
+                Surname = "Cinbek",
+                Email = "tugbacinbek@gmail.com",
+                PhoneNumber = "553-067-7078",
+            };
+
+            Costumer costumer2 = new Costumer 
+            {
+                Id = 2,
+                IdentificationNumber = 222222222,
+                Name = "Sude",
+                Surname = "Kar",
+                Email = "sudekarl47@gmail.com",
+                PhoneNumber = "537-066-5418",
+            };
+           
+
+            CostumerManager costumerManager = new CostumerManager();
+
+            costumerManager.Add(costumer1 );
+            Console.WriteLine(costumer1.Id+costumer1.Name+ " "+ costumer1.Surname);
+
+            costumerManager.List(costumer1);
+            Console.WriteLine(costumer1.Id + costumer1.Name + " " + costumer1.Surname);
+
+            costumerManager.Delete(costumer1);
+            Console.WriteLine(costumer1.Id + costumer1.Name + " "+ costumer1.Surname);
+
+
+            Costumer[] costumers = new Costumer[] { costumer1, costumer2 };
+            foreach (Costumer costumer in costumers)
+            {
+                Console.WriteLine(costumer.Id);
+                Console.WriteLine(costumer.IdentificationNumber);
+                Console.WriteLine(costumer.Name);
+                Console.WriteLine(costumer.Surname);
+                Console.WriteLine(costumer.Email);
+                Console.WriteLine(costumer.PhoneNumber);
+            }
+        }
+    }
+    }
+
