524fc1397a3449692b4085821545dc5773427118
Tugbba <121641171+Tugbba@users.noreply.github.com>
2023-05-09 21:36:35 +0300

    Update Customer.cs

--- ClassMetotDemo/Customer.cs
+++ ClassMetotDemo/Customer.cs
@@ -9,8 +9,8 @@ namespace ClassMetotDemo
     {
         public int Id { get; set; }
         public string TcNo { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
     }
